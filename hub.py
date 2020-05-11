945e40a17b1b4e2fc7660ac2b69da573949271ba
xWasp97x <imirc97@gmail.com>
2020-05-11 13:35:52 +0200

    Switched to in-library bulbs discovery

--- hub.py
+++ hub.py
@@ -2,7 +2,7 @@ import os
 import socket
 import sys
 import time
-from yeelight import Bulb
+from yeelight import Bulb, discover_bulbs
 from pyroute2 import IPRoute
 from loguru import logger
 from tqdm import trange
@@ -47,46 +47,13 @@ class HUB:
 		self.mqtt_client.subscribe(topic=self.mqtt_topic, qos=1)
 		logger.info(f'Subscribed to {self.mqtt_topic}')
 
-	def get_subnet(self):
-		ip_scanner = IPRoute()
-		info = [{'iface': x['index'],
-				 'addr': x.get_attr('IFA_ADDRESS'),
-				 'mask': x['prefixlen']} for x in ip_scanner.get_addr()]
-		ip_scanner.close()
-		logger.debug(f"Available interfaces: {info}")
-		subnet = None
-		for interface in info:
-			if '192.168' in interface['addr']:
-				subnet = f'192.168.{interface["addr"].split(".")[2]}'
-				return subnet
-
 	def get_bulbs_ips(self):
 		logger.info('Scanning network for bulbs...')
+		bulbs = discover_bulbs()
 
-		subnet = self.get_subnet()
-		if subnet is None:
-			logger.error('No router connection! Aborting...')
-			return None
+		bulbs = [{'name': bulb['name'], 'ip': bulb['ip']} for bulb in bulbs]  # [{'hostname': '<>', 'ip': '<>'}]
 
-		#subnet = '192.168.178'
-		logger.debug(f'Subnet: {subnet}')
-
-		bulbs = list()  # [{'hostname': '<>', 'ip': '<>'}]
-
-		progressbar = trange(256, desc='', leave=True)
-		for subnet_ip in progressbar:
-			try:
-				ip = f'{subnet}.{subnet_ip}'
-				progressbar.set_description(f'Scanning {ip}')
-				hostname = socket.gethostbyaddr(ip)[0]
-				if 'yeelink' in hostname:
-					bulbs.append({'hostname': hostname, 'ip': ip})
-					logger.info(f'Found new bulb: {hostname} at {ip}')
-			except socket.herror as se:
-				if 'Unknown host' not in str(se):
-					logger.error(str(se))
-
-		result = [f"hostname: {bulb['hostname']}, ip: {bulb['ip']}" for bulb in bulbs]
+		result = [f"hostname: '{bulb['hostname']}', ip: {bulb['ip']}" for bulb in bulbs]
 		if len(bulbs) > 0:
 			logger.info(f'Network scan ended, result:\n' + '\n'.join(result))
 		else:
